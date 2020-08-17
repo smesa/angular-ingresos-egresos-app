82c7c5921466a67d78b690dd955f2106146d1008
Sergio Mesa <messergio@gmail.com>
2020-08-17 14:02:51 -0500

    Initial commit

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,27 @@
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
+
+const extraOptions: ExtraOptions = {
+  useHash: true
+};
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: dashboardRoutes
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: DashboardComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, extraOptions)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
